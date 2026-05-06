d4068731f68cf263deda1415447abe42253e84ec
rameshsc <rgowda@omnisite.com>
2026-05-06 16:21:29 +0530

    update_image script update to take bakup in ota

--- meta-digi-dey/recipes-digi/swu-images/files/update_images.sh
+++ meta-digi-dey/recipes-digi/swu-images/files/update_images.sh
@@ -20,9 +20,50 @@ if [ $# -lt 1 ]; then
 	exit 1;
 fi
 
+HOME_ROOT_DIR="/home/root"
+PERSISTENT_DATA_DIR="/mnt/data"
+BACKUP_DIR="${PERSISTENT_DATA_DIR}/swupdate-home-root-backup"
+PRESERVED_ITEMS="cert config.json plc_settings.json stats.json"
+
+backup_home_root_files() {
+	echo "Backing up persistent /home/root files to ${BACKUP_DIR}"
+	mkdir -p "${BACKUP_DIR}"
+
+	for item in ${PRESERVED_ITEMS}; do
+		src="${HOME_ROOT_DIR}/${item}"
+		dst="${BACKUP_DIR}/${item}"
+
+		if [ -e "${src}" ]; then
+			rm -rf "${dst}"
+			cp -a "${src}" "${dst}"
+			echo "Backed up ${src} to ${dst}"
+		else
+			echo "Skipping ${src}; file or directory not found"
+		fi
+	done
+}
+
+restore_home_root_files() {
+	echo "Restoring persistent /home/root files from ${BACKUP_DIR}"
+	mkdir -p "${HOME_ROOT_DIR}"
+
+	for item in ${PRESERVED_ITEMS}; do
+		src="${BACKUP_DIR}/${item}"
+		dst="${HOME_ROOT_DIR}/${item}"
+
+		if [ -e "${src}" ]; then
+			rm -rf "${dst}"
+			cp -a "${src}" "${dst}"
+			echo "Restored ${src} to ${dst}"
+		else
+			echo "Skipping ${src}; backup not found"
+		fi
+	done
+}
+
 # Called just before installation process starts.
 if [ "${1}" = "preinst" ]; then
-	:
+	backup_home_root_files
 
 	# TODO: Execute custom code here. For example:
 	# - Mount additional devices/partitions.
@@ -31,7 +72,7 @@ fi
 
 # Called just after installation process ends.
 if [ "${1}" = "postinst" ]; then
-	:
+	restore_home_root_files
 
 	# TODO: Execute custom code here. For example:
 	# - Clean files/directories.
